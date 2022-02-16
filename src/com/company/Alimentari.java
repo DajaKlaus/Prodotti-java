54971a3e38441bda9cb045d654421896c463ad37
DajaKlaus <91118237+DajaKlaus@users.noreply.github.com>
2022-02-16 14:27:10 +0100

    Array prodotti

--- src/com/company/Alimentari.java
+++ src/com/company/Alimentari.java
@@ -4,16 +4,28 @@ import java.util.ArrayList;
 
 public class Alimentari extends Prodotti{
     private Data data;
-    ArrayList <Data> alimentari;
+    private Prodotti prodotti;
+    ArrayList <Data> alimentariS;
+    ArrayList <Prodotti> listaAlimentari;
 
     public Alimentari() {
         super();
         data = new Data();
-        alimentari = new ArrayList<>();
+        prodotti = new Prodotti();
+        alimentariS = new ArrayList<>();
+        listaAlimentari = new ArrayList<>();
+    }
+
+    public void setProdotto(int c, String d, int p) {
+        prodotti.setCodice(c);
+        prodotti.setDescrizione(d);
+        prodotti.setPrezzo(p);
+
+        listaAlimentari.add(prodotti);
     }
 
     public void setData(int i) {
-        alimentari.add(data.getScadenza(i));
+        alimentariS.add(data.getScadenza(i));
     }
 
     public int applicaSconto2() {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList <Alimentari> alimentaris = new ArrayList<>();
+        Alimentari a = new Alimentari();
 
         alimentaris.add(new Alimentari());
+        a.setProdotto(23, "d", 62);
     }
 }
