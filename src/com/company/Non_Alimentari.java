406aa6fd73a5fdd43761c0927a5b1adaba8af451
DajaKlaus <91118237+DajaKlaus@users.noreply.github.com>
2022-02-16 14:55:35 +0100

    materiali

--- src/com/company/Non_Alimentari.java
+++ src/com/company/Non_Alimentari.java
@@ -1,4 +1,38 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Non_Alimentari extends Prodotti{
+    private String materiale;
+    private Prodotti prodotti;
+    ArrayList<Prodotti> listaNonAlimentari;
+
+    public Non_Alimentari() {
+        super();
+        materiale = "";
+        prodotti = new Prodotti();
+        listaNonAlimentari = new ArrayList<>();
+    }
+
+    public String Materiale() {
+        materiale = prodotti.getDescrizione();
+
+        return materiale;
+    }
+
+    public int applicaSconto10() {
+        int sconto = 0;
+
+        sconto = (super.getPrezzo()*10)/100;
+
+        return sconto;
+    }
+
+    public void controlloMateriale() {
+        String mat = Materiale();
+
+        if (mat.equalsIgnoreCase("carta")) {
+
+        }
+    }
 }
